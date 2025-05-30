c271bb803156ecdf267017bcd2de98eef077e324
Pedro Palma <pedro.francisco.palma@gmail.com>
2025-05-29 23:57:55 -0300

    Adicionando telas e OBJ
    Adicionando as telas de cadastro de produtos, cadastro de funcionários e adicionando os objetos + parâmetros de cada um.

---
+++ interface_chicos-main/interface_chicos/interfaceTB/src/main/java/com/mycompany/interfacetb/funcionariosOBJ.java
@@ -0,0 +1,63 @@
+package com.mycompany.interfacetb;
+
+public class funcionariosOBJ {
+    private String nome_func, sexo_func, cpf, cargo, uniforme, vt;
+
+    public String getNome_func() {
+        return nome_func;
+    }
+
+    public void setNome_func(String nome_func) {
+        this.nome_func = nome_func;
+    }
+
+    public String getSexo_func() {
+        return sexo_func;
+    }
+
+    public void setSexo_func(String sexo_func) {
+        this.sexo_func = sexo_func;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getUniforme() {
+        return uniforme;
+    }
+
+    public void setUniforme(String uniforme) {
+        this.uniforme = uniforme;
+    }
+
+    public String getVt() {
+        return vt;
+    }
+
+    public void setVt(String vt) {
+        this.vt = vt;
+    }
+    
+    public String status_func(){
+        return"-----Novo Funcionário!-----"+
+                "\nNome: "+this.nome_func+
+                "\nSexo: "+this.sexo_func+
+                "\nCPF: "+this.cpf+
+                "\nCargo: "+this.cargo+
+                "\nDeseja receber o Uniforme? "+this.uniforme+
+                "\nDeseja receber o Vale Transporte? "+this.vt;
+    }
+}
---
+++ interface_chicos-main/interface_chicos/interfaceTB/src/main/java/com/mycompany/interfacetb/lanches.java
@@ -0,0 +1,64 @@
+package com.mycompany.interfacetb;
+
+public class lanches {
+    private String nome_lanche, tipo_queijo, tipo_carne, tipo_pao, molho, acompanhamento;
+
+    public String getNome_lanche() {
+        return nome_lanche;
+    }
+
+    public void setNome_lanche(String nome_lanche) {
+        this.nome_lanche = nome_lanche;
+    }
+
+    public String getTipo_queijo() {
+        return tipo_queijo;
+    }
+
+    public void setTipo_queijo(String tipo_queijo) {
+        this.tipo_queijo = tipo_queijo;
+    }
+
+    public String getTipo_carne() {
+        return tipo_carne;
+    }
+
+    public void setTipo_carne(String tipo_carne) {
+        this.tipo_carne = tipo_carne;
+    }
+
+    public String getTipo_pao() {
+        return tipo_pao;
+    }
+
+    public void setTipo_pao(String tipo_pao) {
+        this.tipo_pao = tipo_pao;
+    }
+
+    public String getMolho() {
+        return molho;
+    }
+
+    public void setMolho(String molho) {
+        this.molho = molho;
+    }
+
+    public String getAcompanhamento() {
+        return acompanhamento;
+    }
+
+    public void setAcompanhamento(String acompanhamento) {
+        this.acompanhamento = acompanhamento;
+    }
+    
+    
+    public String status_lanche(){
+        return"-----Novo Lanche-----"+
+                "\nNome do Lanche: "+ this.nome_lanche+
+                "\nTipo do queijo: "+ this.tipo_queijo+
+                "\nTipo da carne: "+ this.tipo_carne+
+                "\nTipo do pão: "+ this.tipo_pao+
+                "\nMolho: "+this.molho+
+                "\nAcompanhamento: "+this.acompanhamento;
+    }
+}
